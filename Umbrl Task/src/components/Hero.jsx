1c829d480dc78209b5c89942196ae6b5b946b1c7
Attilio Andrea Marotta <attilio@mekmo.studio>
2024-02-18 15:02:51 +0000

    Add responsive hero section

---
+++ Umbrl Task/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section className="w-full flex  flex-col items-center justify-center">
+      <h2 className="gilroy-bold text-4xl sm:text-6xl mt-24">
+        Pricing & Plans
+      </h2>
+      <p className="girloy-light text-base sm:text-xl sm:w-1/2 text-center p-10 ">
+        With lots of unique blocks, you can easily build a page without coding.
+        <br />
+        Build your next consultancy website within few minutes.
+      </p>
+    </section>
+  )
+}
